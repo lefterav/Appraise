b207352d6c05777d8aec24a33663707ec52efab1
David Vilar <david.vilar@dfki.de>
2012-08-17 18:06:44 +0200

    Added esmt.org "documentation"

--- appraise/resetDB.sh
+++ appraise/resetDB.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-export ESMT_CAMPAIGN=testing
 
 python2 manage.py reset corpus --noinput
 python2 manage.py reset evaluation --noinput
 python2 manage.py syncdb
 ./esmt add language -i ES -l Spanish -n Español
 ./esmt add language -i DE -l German -n Deutsch
-#./esmt add campaign testing
 ./esmt add document test/source-ES -l ES -u
 ./esmt add document test/source-ES-2 -l ES -u
 ./esmt add document test/source-DE -l DE -u
@@ -18,3 +16,4 @@ python2 manage.py syncdb
 ./esmt add translation -i corpus1 -s jane test/translation-corpus1-jane -l DE
 ./esmt add translation -i corpus1 -s moses test/translation-corpus1-moses -l DE
 ./esmt add translation -i corpus1 -s lucy test/translation-corpus1-lucy -l DE
+./esmt add task -t ranking -c corpus1 -s jane,moses,lucy -n fromCmdLine -u david -l DE -R
